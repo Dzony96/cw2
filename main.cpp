1099c60658f3bb57ede724c5df55fffe14bb72fa
Dzony96 <sacha102296@gmail.com>
2017-09-25 16:59:35 +0200

    witam2...

--- main.cpp
+++ main.cpp
@@ -5,10 +5,20 @@ using namespace std;
 int main()
 {
 
-    string imie="Patryk";
-    string nazwisko="Sacharczuk";
-    int rok_urodzenia=1996;
-    int aktualny_rok=2017;
+    string imie;
+    string nazwisko;
+    cout<<"Podaj imie"<<endl;
+    cin>>imie;
+    cout<<"Podaj nazwisko"<<endl;
+    cin>>nazwisko;
+
+    int rok_urodzenia;
+    cout<<"Podaj rok swoj rok urodzenia"<<endl;
+    cin>>rok_urodzenia;
+    int aktualny_rok;
+    cout<<"Podaj aktualny rok"<<endl;
+    cin>>aktualny_rok;
+
     cout<< "Witam, nazywam sie " <<imie<<" " <<nazwisko << " i mam " <<aktualny_rok-rok_urodzenia <<" lat.";
 
     return 0;
