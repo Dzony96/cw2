cf8fce6791f60df36cbcc8b8f14bcd74f314193c
Dzony96 <sacha102296@gmail.com>
2017-09-25 16:50:59 +0200

    mam..

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    string imie="Patryk";
+    string nazwisko="Sacharczuk";
+    int rok_urodzenia=1996;
+    int aktualny_rok=2017;
+    cout<< "Witam, nazywam sie " <<imie<<" " <<nazwisko << " i mam " <<aktualny_rok-rok_urodzenia <<" lat.";
+
     return 0;
 }
